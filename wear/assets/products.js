7ceffa4dedeb7cc100f2e8c3aecc44d452ac64df
Junuxa <rohosshopata@gmail.com>
2026-05-31 12:32:45 +0600

    Update products.js

--- wear/assets/products.js
+++ wear/assets/products.js
@@ -4,6 +4,6 @@
                 desc: "দুর্দান্ত ফিটিং এবং প্রিমিয়াম কটন ফ্যাব্রিকে তৈরি। <mark class='bg-blue-100 text-brand-blue px-1 rounded'>ফরমাল টাইপ এর সেরা কালেকশন।</mark> কালার গ্যারান্টি এবং আরামদায়ক পরিধান নিশ্চিত।",
                 price: 1550, discount: 10, sizes: ["M", "L", "XL", "XXL"],
                 video: "https://www.w3schools.com/html/mov_bbb.mp4", 
-                images: ["https://i.ibb.co/4g3nrwCg/1000173873.jpg", "https://i.ibb.co/nqxVpnLP/1000173875.jpg", "https://i.ibb.co/nqxVpnLP/1000173875.jpg"]
+                images: ["https://i.ibb.co/4g3nrwCg/1000173873.jpg", "https://i.ibb.co/nqxVpnLP/1000173875.jpg", "https://i.ibb.co/Jwp22vfY/1000173874.jpg"]
             }
                     ];
